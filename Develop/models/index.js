124b3420cb43678786742991c49b693824479b6b
ntjohns1 <nelsontjohns@gmail.com>
2021-05-06 00:19:58 -0400

    rebuilding file stucture

---
+++ Develop/models/index.js
@@ -0,0 +1,37 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
